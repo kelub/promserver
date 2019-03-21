7b82684c55ee888f6e2e9374c4175098837f8580
hubing <hubing@stevegame.cn>
2019-03-21 18:59:56 +0800

    add grpc nsq

--- stats/grpc_collector.go
+++ stats/grpc_collector.go
@@ -2,39 +2,58 @@ package stats
 
 import (
 	prom "github.com/prometheus/client_golang/prometheus"
+	"time"
 )
 
 type GrpcCollector struct {
 	namespace string
-	*grpcDesc
+	*grpcVec
 }
 
-type grpcDesc struct {
+type grpcVec struct {
 	//callingGauge	*prom.GaugeVec	//调用中的数量
-	waitGauge    *prom.GaugeVec   //等待中的数量
+	waitGauge    *prom.GaugeVec   //等待中的数量 rpc
 	startCounter *prom.CounterVec //开始rpc计数
 	endCounter   *prom.CounterVec //结束rpc计数
 	errorCounter *prom.CounterVec //执行出错rpc计数
 
-	handledHistogram *prom.HistogramVec //执行直方图
+	handledHistogram *prom.HistogramVec //rpc处理耗时直方图
 }
 
 func NewGrpcCollector() *GrpcCollector {
 	namespace := "grpc"
-	grpcDesc := &grpcDesc{
+	grpcVec := &grpcVec{
 		waitGauge: prom.NewGaugeVec(prom.GaugeOpts{
 			Namespace: namespace,
 			Subsystem: "",
 			Name:      "grpc_wait_total",
 			Help:      "grpc_wait_total",
-		}, []string{"xxx"}),
-		startCounter:     prom.NewCounterVec(),
-		endCounter:       prom.NewCounterVec(),
-		handledHistogram: prom.NewHistogramVec(),
+		}, []string{"service", "service_id", "func_name"}),
+		startCounter: prom.NewCounterVec(prom.CounterOpts{
+			Namespace: namespace,
+			Subsystem: "",
+			Name:      "grpc_start_counter",
+			Help:      "grpc_start_counter",
+		}, []string{"service", "service_id", "func_name"}),
+		endCounter: prom.NewCounterVec(prom.CounterOpts{
+			Namespace: namespace,
+			Subsystem: "",
+			Name:      "grpc_end_counter",
+			Help:      "grpc_end_counter",
+		}, []string{"service", "service_id", "func_name"}),
+		handledHistogram: prom.NewHistogramVec(prom.HistogramOpts{
+			Namespace: namespace,
+			Subsystem: "",
+			Name:      "grpc_handled_histogram",
+			Help:      "grpc_handled_histogram handled time",
+			//Buckets:   prom.DefBuckets,
+			Buckets: []float64{.005, .01, .025, .05, .1, .25, .5, 1, 2.5, 5, 10},
+			//Buckets: prom.LinearBuckets(0.1, .1, 20),
+		}, []string{"service", "service_id", "func_name"}),
 	}
 	return &GrpcCollector{
 		namespace: namespace,
-		grpcDesc:  grpcDesc,
+		grpcVec:   grpcVec,
 	}
 }
 
@@ -52,4 +71,123 @@ func (g *GrpcCollector) Collect(ch chan<- prom.Metric) {
 	g.endCounter.Collect(ch)
 	g.errorCounter.Collect(ch)
 	g.handledHistogram.Collect(ch)
+}
+
+func (g *GrpcCollector) waitGaugeAdd(service string, service_id string, func_name string, counter float64) {
+	g.waitGauge.WithLabelValues(service, service_id, func_name).Add(counter)
+}
+
+func (g *GrpcCollector) waitGaugeInc(service string, service_id string, func_name string) {
+	g.waitGauge.WithLabelValues(service, service_id, func_name).Inc()
+}
+
+func (g *GrpcCollector) waitGaugeDec(service string, service_id string, func_name string) {
+	g.waitGauge.WithLabelValues(service, service_id, func_name).Dec()
+}
+
+func (g *GrpcCollector) startCounterInc(service string, service_id string, func_name string) {
+	g.startCounter.WithLabelValues(service, service_id, func_name).Inc()
+}
+
+func (g *GrpcCollector) startCounterAdd(service string, service_id string, func_name string, counter float64) {
+	g.startCounter.WithLabelValues(service, service_id, func_name).Add(counter)
+}
+
+func (g *GrpcCollector) endCounterInc(service string, service_id string, func_name string) {
+	g.endCounter.WithLabelValues(service, service_id, func_name).Inc()
+}
+
+func (g *GrpcCollector) endCounterAdd(service string, service_id string, func_name string, counter float64) {
+	g.endCounter.WithLabelValues(service, service_id, func_name).Add(counter)
+}
+func (g *GrpcCollector) errorCounterInc(service string, service_id string, func_name string) {
+	g.errorCounter.WithLabelValues(service, service_id, func_name).Inc()
+}
+
+func (g *GrpcCollector) errorCounterAdd(service string, service_id string, func_name string, counter float64) {
+	g.errorCounter.WithLabelValues(service, service_id, func_name).Add(counter)
+}
+
+//Observe time set Seconds
+func (g *GrpcCollector) handledTime(service string, service_id string, func_name string, status string, start time.Time) {
+	g.handledHistogram.WithLabelValues(service, service_id, func_name, status).Observe(time.Since(start).Seconds())
+}
+
+type NsqCollector struct {
+	namespace string
+	*nsqVec
+}
+
+type nsqVec struct {
+	errorPingCounter *prom.CounterVec //Ping失败计数
+	waitPubGauge     *prom.GaugeVec   //等待中的发布数量
+	waitSubGauge     *prom.GaugeVec   //等待中的订阅数量
+
+	startSubCounter     *prom.CounterVec
+	endSubCounter       *prom.CounterVec
+	errorSubCounter     *prom.CounterVec
+	handledSubHistogram *prom.HistogramVec //Sub处理耗时直方图
+}
+
+func NewNsqCollector() *NsqCollector {
+	namespace := "nsq"
+	nsqVec := &nsqVec{
+		errorPingCounter: prom.NewCounterVec(prom.CounterOpts{
+			Namespace: namespace,
+			Subsystem: "",
+			Name:      "nsq_ping_error_counter",
+			Help:      "nsq_ping_error_counter",
+		}, []string{"service", "topic"}),
+		waitPubGauge: prom.NewGaugeVec(prom.GaugeOpts{
+			Namespace: namespace,
+			Subsystem: "",
+			Name:      "nsq_waitPub_gauge",
+			Help:      "nsq_waitPub_gauge",
+		}, []string{"service", "topic"}),
+		waitSubGauge: prom.NewGaugeVec(prom.GaugeOpts{
+			Namespace: namespace,
+			Subsystem: "",
+			Name:      "nsq_waitSub_gauge",
+			Help:      "nsq_waitSub_gauge",
+		}, []string{"service", "topic"}),
+		startSubCounter: prom.NewCounterVec(prom.CounterOpts{
+			Namespace: namespace,
+			Subsystem: "",
+			Name:      "nsq_startSub_counter",
+			Help:      "nsq_startSub_counter",
+		}, []string{"service", "topic", "channel"}),
+		endSubCounter: prom.NewCounterVec(prom.CounterOpts{
+			Namespace: namespace,
+			Subsystem: "",
+			Name:      "nsq_endSub_counter",
+			Help:      "nsq_endSub_counter",
+		}, []string{"service", "topic", "channel"}),
+		errorSubCounter: prom.NewCounterVec(prom.CounterOpts{
+			Namespace: namespace,
+			Subsystem: "",
+			Name:      "nsq_errorSub_counter",
+			Help:      "nsq_errorSub_counter",
+		}, []string{"service", "topic", "channel"}),
+		handledSubHistogram: prom.NewHistogramVec(prom.HistogramOpts{
+			Namespace: namespace,
+			Subsystem: "",
+			Name:      "nsq_handledSub_histogram",
+			Help:      "nsq_handledSub_histogram handled time",
+			//Buckets:   prom.DefBuckets,
+			Buckets: []float64{.005, .01, .025, .05, .1, .25, .5, 1, 2.5, 5, 10},
+			//Buckets: prom.LinearBuckets(0.1, .1, 20),
+		}, []string{"service", "topic", "channel"}),
+	}
+	return &NsqCollector{
+		namespace: namespace,
+		nsqVec:    nsqVec,
+	}
+}
+
+func (n *NsqCollector) Describe(ch chan<- *prom.Desc) {
+
+}
+
+func (n *NsqCollector) Collect(ch chan<- prom.Metric) {
+
 }
--- stats/stats.go
+++ stats/stats.go
@@ -2,14 +2,94 @@ package stats
 
 import (
 	"context"
-
+	"github.com/Sirupsen/logrus"
+	"github.com/prometheus/client_golang/prometheus"
 	"google.golang.org/grpc"
+	"net/http"
+	"time"
 )
+
+type rpcMonitor struct {
+	service    string
+	name       string
+	service_id string
+	collector  *GrpcCollector
+	startTime  time.Time
+}
+
+func NewRpcMonitor(service string, name string, service_id string) *rpcMonitor {
+	logrus.Infof("NewRpcMonitor %s", name)
+	m := &rpcMonitor{
+		service:    service,
+		name:       name,
+		service_id: service_id,
+	}
+	m.startTime = time.Now()
+	return m
+}
+
+func (m *rpcMonitor) WaitGaugeInc() {
+	m.collector.waitGaugeInc(m.service, m.service_id, m.name)
+}
+
+func (m *rpcMonitor) WaitGaugeDec() {
+	m.collector.waitGaugeDec(m.service, m.service_id, m.name)
+}
+
+func (m *rpcMonitor) WaitGaugeAdd(value float64) {
+	m.collector.waitGaugeAdd(m.service, m.service_id, m.name, value)
+}
+
+func (m *rpcMonitor) StartCounterInc() {
+	m.collector.startCounterInc(m.service, m.service_id, m.name)
+}
+
+func (m *rpcMonitor) EndCounterInc() {
+	m.collector.startCounterInc(m.service, m.service_id, m.name)
+}
+
+func (m *rpcMonitor) ErrorCounterInc() {
+	m.collector.startCounterInc(m.service, m.service_id, m.name)
+}
+
+// TODO 具体错误标示
+func (m *rpcMonitor) HandledEnd(status string) {
+	m.collector.handledTime(m.service, m.service_id, m.name, status, m.startTime)
+}
 
 // gRPC 拦截器
 func UnaryServerInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
-
+	monitor := NewRpcMonitor("service_test", "77", info.FullMethod)
+	monitor.WaitGaugeInc()
+	monitor.StartCounterInc()
 	resp, err = handler(ctx, req)
-
+	monitor.EndCounterInc()
+	monitor.WaitGaugeDec()
+	if err != nil {
+		monitor.ErrorCounterInc()
+		monitor.HandledEnd("failed")
+	}
+	monitor.HandledEnd("succeed")
 	return resp, err
+}
+
+// 运行
+func StartPromServer() {
+	ListenAddr := ":9102"
+	prometheus.MustRegister(NewGrpcCollector())
+	prometheus.MustRegister(NewNsqCollector())
+	http.Handle("/metrics", prometheus.Handler())
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`<html>
+			<head><title>Nsq Exporter</title></head>
+			<body>
+			<h1>Node Exporter</h1>
+			<p><a href="` + "/metrics" + `">Metrics</a></p>
+			</body>
+			</html>`))
+	})
+	logrus.Infof("Listening on %s", ListenAddr)
+	if err := http.ListenAndServe(ListenAddr, nil); err != nil {
+		logrus.WithError(err).Fatalln("启动失败", err)
+	}
 }
